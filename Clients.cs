84a247030380272b664fffc963b120a27cfc4240
RadoslavKolev <radoslav.kolev@abv.bg>
2022-03-31 19:36:36 +0300

    6.1. Save to .txt functionality added

--- Clients.cs
+++ Clients.cs
@@ -63,9 +63,69 @@ namespace Sugar_Factory
 
         }
 
+        private void DataTableToTextFile(DataTable dt, string outputFilePath)
+        {
+            int[] maxLengths = new int[dt.Columns.Count];
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                maxLengths[i] = dt.Columns[i].ColumnName.Length;
+                foreach (DataRow row in dt.Rows)
+                {
+                    if (!row.IsNull(i))
+                    {
+                        int length = row[i].ToString().Length;
+                        if (length > maxLengths[i])
+                            maxLengths[i] = length;
+                    }
+                }
+            }
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(outputFilePath, false))
+                {
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                        sw.Write(dt.Columns[i].ColumnName.PadRight(maxLengths[i] + 2));
+
+                    sw.WriteLine();
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        for (int i = 0; i < dt.Columns.Count; i++)
+                        {
+                            if (!row.IsNull(i))
+                                sw.Write(row[i].ToString().PadRight(maxLengths[i] + 2));
+                            else
+                                sw.Write(new string(' ', maxLengths[i] + 2));
+                        }
+                        sw.WriteLine();
+                    }
+                    sw.Close();
+                }
+            }
+            catch { }
+        }
+
         private void button_SaveToTxtClick(object sender, EventArgs e)
         {
+            string connectionString = null;
+            connectionString = menu.connection;
 
+            DataTable dt = new DataTable();
+            foreach (DataGridViewTextBoxColumn column in dataGridView1.Columns)
+                dt.Columns.Add(column.Name, column.ValueType);
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                DataRow dr = dt.NewRow();
+                foreach (DataGridViewTextBoxColumn column in dataGridView1.Columns)
+                    if (row.Cells[column.Name].Value != null)
+                        dr[column.Name] = row.Cells[column.Name].Value.ToString();
+                dt.Rows.Add(dr);
+            }
+
+            string filePath = "D:\\ТУ-Варна\\Семестър 8\\Информационни системи\\Sugar Factory\\Sugar Factory\\queires\\Clients.txt";
+            DataTableToTextFile(dt, filePath);
+            MessageBox.Show("Data saved successfully!", "Data saved!");
         }
 
         private void button_BackToMainClick(object sender, EventArgs e)
